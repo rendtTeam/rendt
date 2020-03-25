02845e769348486f1dc83cd42a04353892456e21
alucaerd <cenk.er98@gmail.com>
2020-03-25 05:24:52 +0300

    now python container will run the first added file from the files list and copies every file given into the container

--- src/client/createdocker.py
+++ src/client/createdocker.py
@@ -1,38 +1,42 @@
 from tkinter import *
 import os
 
-import ui
-"""
-def createfilesdir():
+from ui import filelist
+
+def createfilesdir(filelist):
     workd = "" + os.getcwd() + "/files"
     home_dir = os.system("mkdir " + workd )
-    home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/readdocker.py " + workd )
+    for i in filelist:
+        home_dir = os.system("cp /" + i + workd )
+
 def forjava():
+    f = open("Dockerfile", "a")
     f.write('FROM java:8-jdk-alpine\n')
     f.write('COPY \n')
     f.write('WORKDIR /usr/app\n')
     f.write('ENTRYPOINT ["java", "-jar", ""]\n')
     f.close()
 
-def forpython(filename): 
+def forpython(filelist): 
     f = open("Dockerfile", "a")
-    f.write('FROM python:3\n')
+    f.write('FROM python\n')
     f.write('ADD /files /\n')
-    f.write('CMD [ "python", "./readdocker.py" ]\n') 
+    f.write('CMD python '+ os.path.basename(filelist[0]) + '\n') 
 
 
     f.close()
-    home_dir = os.system("rm -R files")
+
     home_dir = os.system("docker build -t rendt .")
     home_dir = os.system("docker run rendt")
-
-
+    home_dir = os.system("rm -R files")
+    home_dir = os.system("rm Dockerfile")
     p = os.popen("docker system prune", "w")
     p.write("y\n")
     home_dir = os.system("docker rmi -f rendt")
     p.write("exit")
 
 def forc():
+    f = open("Dockerfile", "a")
     f.write('FROM ubuntu:latest\n')
     f.write('RUN apt-get -y update && apt-get install -y\n')
     f.write('RUN apt-get -y install clang\n')
@@ -41,35 +45,13 @@ def forc():
     f.write('RUN clang++ -o Test Test.cpp\n')
     f.write('CMD ["./Test"]\n')
     f.close()
+
     home_dir = os.system("docker build -t rendt .")
     home_dir = os.system("docker run rendt")
-
-
+    home_dir = os.system("rm -R files")
+    home_dir = os.system("rm Dockerfile")
     p = os.popen("docker system prune", "w")
     p.write("y\n")
     home_dir = os.system("docker rmi -f rendt")
     p.write("exit")
 
-"""
-
-workd = "" + os.getcwd() + "/files"
-home_dir = os.system("mkdir " + workd )
-home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/readdocker.py " + workd )
-home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/cenk.py " + workd )
-print(workd)
-f = open("Dockerfile", "a")
-f.write('FROM python\n')
-f.write('ADD /files /\n')
-f.write('CMD python readdocker.py\n') 
-
-
-f.close()
-
-home_dir = os.system("docker build -t rendt .")
-home_dir = os.system("docker run rendt")
-home_dir = os.system("rm -R files")
-home_dir = os.system("rm Dockerfile")
-p = os.popen("docker system prune", "w")
-p.write("y\n")
-home_dir = os.system("docker rmi -f rendt")
-p.write("exit")
