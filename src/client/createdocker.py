2c7dbea12a2b77409b38c1bc589b1838a3a42e34
alucaerd <cenk.er98@gmail.com>
2020-03-25 04:33:55 +0300

    made some changes on python filing

--- src/client/createdocker.py
+++ src/client/createdocker.py
@@ -1,21 +1,36 @@
 from tkinter import *
 import os
-f = open("Dockerfile", "a")
-"""def forjava():
+
+import ui
+"""
+def createfilesdir():
+    workd = "" + os.getcwd() + "/files"
+    home_dir = os.system("mkdir " + workd )
+    home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/readdocker.py " + workd )
+def forjava():
     f.write('FROM java:8-jdk-alpine\n')
     f.write('COPY \n')
     f.write('WORKDIR /usr/app\n')
     f.write('ENTRYPOINT ["java", "-jar", ""]\n')
     f.close()
-"""
-def forpython():
+
+def forpython(filename): 
+    f = open("Dockerfile", "a")
     f.write('FROM python:3\n')
-    f.write('ADD test.py /\n')
-    f.write('RUN pip install pystrich\n')
-    f.write('CMD [ "python", "./test.py" ]\n')
-    f.write('CMD ls\n')
+    f.write('ADD /files /\n')
+    f.write('CMD [ "python", "./readdocker.py" ]\n') 
+
 
     f.close()
+    home_dir = os.system("rm -R files")
+    home_dir = os.system("docker build -t rendt .")
+    home_dir = os.system("docker run rendt")
+
+
+    p = os.popen("docker system prune", "w")
+    p.write("y\n")
+    home_dir = os.system("docker rmi -f rendt")
+    p.write("exit")
 
 def forc():
     f.write('FROM ubuntu:latest\n')
@@ -26,15 +41,35 @@ def forc():
     f.write('RUN clang++ -o Test Test.cpp\n')
     f.write('CMD ["./Test"]\n')
     f.close()
+    home_dir = os.system("docker build -t rendt .")
+    home_dir = os.system("docker run rendt")
 
 
+    p = os.popen("docker system prune", "w")
+    p.write("y\n")
+    home_dir = os.system("docker rmi -f rendt")
+    p.write("exit")
+
+"""
+
+workd = "" + os.getcwd() + "/files"
+home_dir = os.system("mkdir " + workd )
+home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/readdocker.py " + workd )
+home_dir = os.system("cp /Users/cenker/Documents/CS/cs492/try/cenk.py " + workd )
+print(workd)
+f = open("Dockerfile", "a")
+f.write('FROM python\n')
+f.write('ADD /files /\n')
+f.write('CMD python readdocker.py\n') 
+
+
+f.close()
 
 home_dir = os.system("docker build -t rendt .")
 home_dir = os.system("docker run rendt")
-
-
+home_dir = os.system("rm -R files")
+home_dir = os.system("rm Dockerfile")
 p = os.popen("docker system prune", "w")
 p.write("y\n")
 home_dir = os.system("docker rmi -f rendt")
 p.write("exit")
-
