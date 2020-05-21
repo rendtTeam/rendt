139960a922be817ddfbf6929a3fc65483e9389b0
alucaerd <cenk.er98@gmail.com>
2020-05-21 16:10:12 +0300

    new docker features

--- src/client/createdocker.py
+++ src/client/createdocker.py
@@ -2,18 +2,23 @@ import os, sys
 
 path_to_output = 'sender_output.txt'
 
+
 f = open("Dockerfile", "a")
 
-f.write('FROM python\n') 
+f.write('FROM gcc\n')
 f.write('FROM java:8-jdk-alpine\n')
-f.write('FROM ubuntu\n') 
+f.write('FROM python\n') 
+f.write('FROM ubuntu:latest\n') 
 f.write('RUN apt update && apt install -y zip\n')
-f.write('RUN apt-get -y update && apt-get install -y\n')
-f.write('RUN apt-get -y install clang\n')
+f.write('RUN apt install -y python\n')
+#f.write('RUN apt install -y python3.7\n')
+f.write('RUN apt install -y default-jdk\n')
+f.write('RUN apt-get install -y gcc\n')
+f.write('RUN apt install -y python3-pip\n')
 f.write('ADD /files.zip /\n')
 f.write('RUN unzip files.zip && rm files.zip\n')
-f.write('ADD /commands.txt /\n')
-f.write('RUN chmod +x run.sh')
+f.write('RUN chmod +x run.sh\n')
+
 
 f.close()
 
@@ -21,11 +26,18 @@ home_dir = os.system("docker build -t rendt .")
 home_dir = os.system("docker run -it -d --name rendtcont rendt")
 home_dir = os.system("rm Dockerfile") 
 
+home_dir = os.system("docker run -it -d --name rendtcont rendt")
+home_dir = os.system("docker cp /Users/cenker/Documents/CS/cs492/rendt/src/client/files.zip rendtcont:/ ")
+home_dir = os.system("docker exec -it rendtcont bash -c 'unzip files.zip && rm files.zip'")
+home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && chmod +x run.sh'")
 a = './run.sh >> sender_output.txt'
 print(a)
-b = "docker exec -it rendtcont bash -c '" + a + "'"
-
-home_dir = os.system("docker cp rendtcont:/sender_output.txt " + path_to_output)
+b = "docker exec -it rendtcont bash -c 'cd files && " + a + "'"
+home_dir = os.system(b) 
+home_dir = os.system("docker exec -it rendtcont bash -c 'mv /files/sender_output.txt /files/output/sender_output.txt'") 
+home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && zip -r -X output.zip output'") 
+home_dir = os.system("docker cp rendtcont:/files/output.zip .")
+home_dir = os.system("docker exec -it rendtcont bash -c 'rm -R files'")
 home_dir = os.system("docker stop rendtcont")
 home_dir = os.system("docker container rm rendtcont")
 
--- src/client/receiver.py
+++ src/client/receiver.py
@@ -91,16 +91,16 @@ class Receiver:
         # execute job
         f = open("Dockerfile", "a")
 
-        f.write('FROM python\n') 
+
+        f.write('FROM gcc\n')
         f.write('FROM java:8-jdk-alpine\n')
-        f.write('FROM ubuntu\n') 
+        f.write('FROM ubuntu:latest\n') 
+        f.write('FROM python\n') 
         f.write('RUN apt update && apt install -y zip\n')
-        f.write('RUN apt-get -y update && apt-get install -y\n')
-        f.write('RUN apt-get -y install clang\n')
         f.write('ADD /files.zip /\n')
         f.write('RUN unzip files.zip && rm files.zip\n')
-        # f.write('ADD /commands.txt /\n')
-        f.write('RUN chmod +x run.sh')
+        #f.write('RUN chmod +x run.sh\n')
+
 
         f.close()
 
@@ -108,13 +108,16 @@ class Receiver:
         home_dir = os.system("docker run -it -d --name rendtcont rendt")
         home_dir = os.system("rm Dockerfile") 
 
+
+        home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && chmod +x run.sh'")
         a = './run.sh >> sender_output.txt'
         print(a)
-        b = "docker exec -it rendtcont bash -c '" + a + "'"
-
+        b = "docker exec -it rendtcont bash -c 'cd files && " + a + "'"
         home_dir = os.system(b) 
-
-        home_dir = os.system("docker cp rendtcont:/sender_output.txt " + path_to_output)
+        home_dir = os.system("docker exec -it rendtcont bash -c 'mv /files/sender_output.txt /files/output/sender_output.txt'") 
+        home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && zip -r -X output.zip output'") 
+        #home_dir = os.system("docker cp rendtcont:/sender_output.txt " + path_to_output)
+        home_dir = os.system("docker cp rendtcont:/files/output.zip " + path_to_output)
         home_dir = os.system("docker stop rendtcont")
         home_dir = os.system("docker container rm rendtcont")
 
