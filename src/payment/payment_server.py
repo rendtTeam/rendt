f7669d37e63fc862d3faee34afeace4e0ef61292
Nurlan Farzali <fnurlan@outlook.com>
2020-05-27 00:28:37 +0300

    create_payment method modified, message response method added

--- src/payment/payment_server.py
+++ src/payment/payment_server.py
@@ -21,26 +21,84 @@ def calculate_order_amount(items):
     # Replace this constant with a calculation of the order's amount
     # Calculate the order total on the server to prevent
     # people from directly manipulating the amount on the client
-    return 1400
+    """get date time from db calculate total amount of elapsed time"""
+    return 10000
 
 
 @app.route('/create-payment-intent', methods=['POST'])
 def create_payment():
     try:
-        data = json.loads(request.data)
+        data = request.get_json()
+        pprint.pprint(data)
         intent = stripe.PaymentIntent.create(
             amount=calculate_order_amount(data['items']),
-            currency='usd'
+            currency='usd',
+            receipt_email = "fnurlan7@gmail.com",
+            # payment_method = 
         )
-
-        # return jsonify({
-        #   'clientSecret': intent['client_secret']
-        # })
+        print("Output check start----------------------:  ")
+        # pprint.pprint(intent)
+        # returnedResponse = generate_response(intent)
+        # print(type(returnedResponse)
+        # print(intent)
+        pprint.pprint(generate_response(intent))
+        print("Output check finish----------------------:  ")
         return jsonify({
-            'publishableKey': os.getenv('STRIPE_PUBLISHABLE_KEY'), 'clientSecret': intent.client_secret
+          'clientSecret': intent['client_secret']
         })
-    except Exception as e:
-        return jsonify(error=str(e)), 403
+    # except Exception as e:
+    #     return jsonify(error=str(e)), 404
+    except stripe.error.CardError as e:
+        print(e.user_message)
+        return jsonify({'error': e.user_message})
+
+
+# @app.route('/create-payment-intent', methods=['POST'])
+# def create_payment():
+#     data = request.get_json()
+#     try:
+#         if 'paymentMethodId' in data:
+#             order_amount = calculate_order_amount(data['items'])
+
+#             # Create new PaymentIntent with a PaymentMethod ID from the client.
+#             intent = stripe.PaymentIntent.create(
+#                 amount=order_amount,
+#                 currency=data['currency'],
+#                 payment_method=data['paymentMethodId'],
+#                 confirmation_method='manual',
+#                 confirm=True,
+#                 # If a mobile client passes `useStripeSdk`, set `use_stripe_sdk=true`
+#                 # to take advantage of new authentication features in mobile SDKs.
+#                 use_stripe_sdk=True if 'useStripeSdk' in data and data['useStripeSdk'] else None,
+#             )
+#             # After create, if the PaymentIntent's status is succeeded, fulfill the order.
+#         elif 'paymentIntentId' in data:
+#             # Confirm the PaymentIntent to finalize payment after handling a required action
+#             # on the client.
+#             intent = stripe.PaymentIntent.confirm(data['paymentIntentId'])
+#             # After confirm, if the PaymentIntent's status is succeeded, fulfill the order.
+
+#         return generate_response(intent)
+#     except stripe.error.CardError as e:
+#         return jsonify({'error': e.user_message})
+
+
+def generate_response(intent):
+    status = intent['status']
+    if status == 'requires_action' or status == 'requires_source_action':
+        # Card requires authentication
+        print(status)
+        return jsonify({'requiresAction': True, 'paymentIntentId': intent['id'], 'clientSecret': intent['client_secret']})
+    elif status == 'requires_payment_method' or status == 'requires_source':
+        # Card was not properly authenticated, suggest a new payment method
+        print(status)
+        return jsonify({'error': 'Your card was denied, please provide a new payment method'})
+    elif status == 'succeeded':
+        # Payment is complete, authentication not required
+        # To cancel the payment you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+        print("💰 Payment received!")
+        return jsonify({'clientSecret': intent['client_secret']})
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook_received():
