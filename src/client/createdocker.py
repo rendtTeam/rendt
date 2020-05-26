32dd9f202560ef2ad9b542eb313a13d5616cbe86
alucaerd <cenk.er98@gmail.com>
2020-05-27 02:37:20 +0300

    added docker build function and dockerfile

--- src/client/createdocker.py
+++ src/client/createdocker.py
@@ -20,28 +20,5 @@ f.close()
 
 home_dir = os.system("docker build -t rendt .")
 
-home_dir = os.system("rm Dockerfile")
+#home_dir = os.system("rm Dockerfile")
 ###############################################################
-home_dir = os.system("pwd>pwd.txt")
-f = open("pwd.txt", "r")
-x = f.readline()
-x = x.split("/")        
-y = "/" + x[1] + "/" + x[2] + "/rendt"
-home_dir = os.system("rm pwd.txt")
-home_dir = os.system("mkdir " + y)
-home_dir = os.system("docker run -it -d --name rendtcont rendt")
-home_dir = os.system("docker cp " + y + "/files.zip rendtcont:/ ")
-home_dir = os.system("docker exec -it rendtcont bash -c 'unzip files.zip && rm files.zip'")
-home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && chmod +x run.sh'")
-a = './run.sh >> sender_output.txt'
-print(a)
-b = "docker exec -it rendtcont bash -c 'cd files && " + a + "'"
-home_dir = os.system(b) 
-home_dir = os.system("docker exec -it rendtcont bash -c 'mv /files/sender_output.txt /files/output/sender_output.txt'") 
-home_dir = os.system("docker exec -it rendtcont bash -c 'cd files && zip -r -X output.zip output'") 
-home_dir = os.system("docker cp rendtcont:/files/output.zip " + y)
-home_dir = os.system("docker exec -it rendtcont bash -c 'rm -R files'")
-home_dir = os.system("docker stop rendtcont")
-home_dir = os.system("docker container rm rendtcont")
-
-home_dir = os.system("cat " + path_to_output)
